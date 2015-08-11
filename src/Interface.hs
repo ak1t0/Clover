33b87c174065248fc1a5ee9532479d94a23f4016
ak1t0 <aktoo3097@gmail.com>
2015-08-12 02:22:06 +0900

    add some function for reading source

--- src/Interface.hs
+++ src/Interface.hs
@@ -1,13 +1,15 @@
 module Interface where
 
-reader :: String -> String -> [Int] -> String
+import Data.List
+
+splitSource :: String -> String -> [Int] -> [String]
 -- check all string
-reader "" res p = if (head p) == (last p)
-  then res ++ (show p)
-  else "Error"
-reader t res p = if ((head p) == (last p) && (head p) /= 0)
-  then res ++ (show p)
-  else reader (tail t) (res ++ [ch]) (check ch p)
+splitSource "" res p = if (head p) == (last p)
+  then [res ++ (show p)]
+  else ["Error"]
+splitSource t res p = if ((head p) == (last p) && (head p) /= 0)
+  then [res ++ (show p), t]
+  else splitSource (tail t) (res ++ [ch]) (check ch p)
   where
     ch = head t
 -- check
@@ -16,3 +18,13 @@ check ch [l, r] = case ch of
   '(' -> [l+1, r]
   ')' -> [l, r+1]
   _ -> [l, r]
+
+search :: Char -> String -> Maybe Int
+search ch str = elemIndex ch str
+
+checkParentLR :: String -> Bool
+checkParentLR str = search '(' str < search ')' str
+
+cleanSource :: String -> String
+cleanSource str =
+  map (\x -> if ((x == '\n') || (x == '\t')) then ' '; else x) str
