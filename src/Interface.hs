4a1345c8971c471a944694f058ea69e3862ce3ef
ak1t0 <aktoo3097@gmail.com>
2015-08-12 01:30:16 +0900

    fix comment and improve reader

--- src/Interface.hs
+++ src/Interface.hs
@@ -1,14 +1,18 @@
 module Interface where
 
 reader :: String -> String -> [Int] -> String
-reader "" res p = res ++ (show p)
-reader t res p = reader (tail t) (res ++ [ch]) (check ch p)
+-- check all string
+reader "" res p = if (head p) == (last p)
+  then res ++ (show p)
+  else "Error"
+reader t res p = if ((head p) == (last p) && (head p) /= 0)
+  then res ++ (show p)
+  else reader (tail t) (res ++ [ch]) (check ch p)
   where
     ch = head t
--- chekc
+-- check
 check :: Char -> [Int] -> [Int]
 check ch [l, r] = case ch of
   '(' -> [l+1, r]
   ')' -> [l, r+1]
   _ -> [l, r]
-a
